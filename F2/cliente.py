ecaa2318ed35fa66e7db0c1c0737b1d293fe8bf8
Berta <nosarabs@gmail.com>
2019-03-23 10:03:58 -0600

    Avance Validacion IP
    Hace falta preguntar si es localhost

--- F2/cliente.py
+++ F2/cliente.py
@@ -1,32 +1,43 @@
-#!/usr/bin/env python
+import sys # Para pasar argumentos
+import re # Para usar RegEx (Expresiones Regulares)
 
-#Variables
+ip = str(sys.argv[1])
+regex = r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+x = re.search(regex, ip)
+try:
+    print("Dirección IP: ", x.group())
+except:
+  print("Dirección IP Invalida")
+
+
+
+# Variables
 host = 'localhost'
 port = 8050
-#Se importa el módulo
+# Se importa el módulo
 import socket
- 
-#Creación de un objeto socket (lado cliente)
+
+# Creación de un objeto socket (lado cliente)
 obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-#Conexión con el servidor. Parametros: IP (puede ser del tipo 192.168.1.1 o localhost), Puerto
+
+# Conexión con el servidor. Parametros: IP (puede ser del tipo 192.168.1.1 o localhost), Puerto
 obj.connect((host, port))
 print("Conectado al servidor")
- 
-#Creamos un bucle para retener la conexion
+
+# Creamos un bucle para retener la conexion
 while True:
-    #Instanciamos una entrada de datos para que el cliente pueda enviar mensajes
-    print("Escriba el mensaje que desea enviar al servidor>> ")
+    # Instanciamos una entrada de datos para que el cliente pueda enviar mensajes
+    print('Escriba el mensaje que desea enviar al servidor: ')
     mens = input()
-    if mens=="1":
+    if mens == "1":
         obj.sendall(mens.encode('utf-8'))
         obj.close()
         break
     else:
-    #Con el método send, enviamos el mensaje
+        # Con el método send, enviamos el mensaje
         obj.sendall(mens.encode('utf-8'))
 
-#Cerramos la instancia del objeto servidor
+# Cerramos la instancia del objeto servidor
 
-#Imprimimos la palabra Adios para cuando se cierre la conexion
-print("Conexión cerrada") 
+# Imprimimos la palabra Adios para cuando se cierre la conexion
+print("Conexión cerrada")
--- F2/servidor.py
+++ F2/servidor.py
@@ -1,32 +1,31 @@
-#!/usr/bin/env python
- 
-#Se importa el módulo
+
+# Se importa el módulo
 import socket
- 
-#instanciamos un objeto para trabajar con el socket
+
+# instanciamos un objeto para trabajar con el socket
 ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-#Puerto y servidor que debe escuchar
+
+# Puerto y servidor que debe escuchar
 ser.bind(("localhost", 8050))
- 
-#Aceptamos conexiones entrantes con el metodo listen. Por parámetro las conexiones simutáneas.
+
+# Aceptamos conexiones entrantes con el metodo listen. Por parámetro las conexiones simutáneas.
 ser.listen(1)
- 
-#Instanciamos un objeto cli (socket cliente) para recibir datos
+
+# Instanciamos un objeto cli (socket cliente) para recibir datos
 cli, addr = ser.accept()
 
-seguir= True
+seguir = True
 while seguir:
-    #Recibimos el mensaje, con el metodo recv recibimos datos. Por parametro la cantidad de bytes para recibir
+    # Recibimos el mensaje, con el metodo recv recibimos datos. Por parametro la cantidad de bytes para recibir
     recibido = cli.recv(1024)
-    if recibido.decode() !="1":
-	#Si se reciben datos nos muestra la IP y el mensaje recibido
-        print ("Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]))
-        print (recibido.decode())
-	#Devolvemos el mensaje al cliente
+    if recibido.decode() != "1":
+        # Si se reciben datos nos muestra la IP y el mensaje recibido
+        print("Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]))
+        print(recibido.decode())
+        # Devolvemos el mensaje al cliente
         cli.send(recibido)
-    if recibido.decode()=="1":
-         print("conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1])+" ha sido cerrada") 
-         #cli.close()
-         #Ser.close()
-         seguir=False
+    if recibido.decode() == "1":
+        print("conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]) + " ha sido cerrada")
+        # cli.close()
+        # Ser.close()
+        seguir = False
