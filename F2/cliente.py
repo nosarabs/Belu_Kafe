0876b6d7af50c354265c369a9743ef7c06136658
FelipeSJ <felipesolisjimenez@hotmail.com>
2019-03-23 23:41:30 -0600

    hilo recibidor del servidor, falta hilo contador

--- F2/cliente.py
+++ F2/cliente.py
@@ -1,43 +1,81 @@
 import sys # Para pasar argumentos
 import re # Para usar RegEx (Expresiones Regulares)
-
-ip = str(sys.argv[1])
-regex = r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
-x = re.search(regex, ip)
-try:
-    print("Dirección IP: ", x.group())
-except:
-  print("Dirección IP Invalida")
-
-
-
-# Variables
-host = 'localhost'
-port = 8050
-# Se importa el módulo
 import socket
+from threading import Thread
+import time
+from queue import Queue
+import threading
+import sys
+
+host=""
+port=0
+if len(sys.argv) > 2:
+    ip = str(sys.argv[1])
+    puerto= str(sys.argv[2])
+    regex = r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+    x = re.search(regex, ip)
+    try:
+        if ip=="localhost":
+           host=ip
+           port=int(puerto)
+        else:   
+           print("Dirección IP: ", x.group())
+           host=ip
+           port=int(puerto)
+    except:
+        print("Dirección IP Invalida")
+        sys.exit(0)
+else:
+    print("No ingresó argumentos: ")
+    print("Debe ingresar ip y puerto en los argumentos")
+    sys.exit(0)
+    
+    
+colaMensajes = Queue()
+semaforo = threading.Semaphore(0)
 
 # Creación de un objeto socket (lado cliente)
 obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Conexión con el servidor. Parametros: IP (puede ser del tipo 192.168.1.1 o localhost), Puerto
+# Conexión con el servidor
 obj.connect((host, port))
-print("Conectado al servidor")
+print("Conectado al contador")
 
-# Creamos un bucle para retener la conexion
-while True:
+# Creamos un bucle para seguir obteniendo datos, se sale cuando se digite un "1"
+def leer():
+    while True:
     # Instanciamos una entrada de datos para que el cliente pueda enviar mensajes
-    print('Escriba el mensaje que desea enviar al servidor: ')
-    mens = input()
-    if mens == "1":
-        obj.sendall(mens.encode('utf-8'))
-        obj.close()
-        break
-    else:
-        # Con el método send, enviamos el mensaje
-        obj.sendall(mens.encode('utf-8'))
+        print('Escriba el mensaje que desea enviar al contador: ')
+        mens = input()
+        if mens == "1":
+            # Con el método put, encolamos el mensaje            
+            colaMensajes.put(mens)
+            #Luego liberamos el semáforo
+            semaforo.release()
+            break
+        else:
+            # Con el método put, encolamos el mensaje
+            colaMensajes.put(mens)
+            #Luego liberamos el semáforo
+            semaforo.release()
 
-# Cerramos la instancia del objeto servidor
+def enviar():
+    while True:
+        #Adquirimos el semáforo
+        semaforo.acquire()
+        mensaje = colaMensajes.get()
+        if mensaje != "1":
+            obj.sendall(mensaje.encode('utf-8')) 
+        else:
+            obj.sendall(mensaje.encode('utf-8'))
+            print("Conexión cerrada")
+            break
 
-# Imprimimos la palabra Adios para cuando se cierre la conexion
-print("Conexión cerrada")
+
+hiloLector = Thread(target=leer, args=())
+hiloEnviador = Thread(target=enviar, args=())
+
+hiloLector.start()
+hiloEnviador.start()
+
+colaMensajes.join()
--- F2/servidor.py
+++ F2/servidor.py
@@ -1,7 +1,41 @@
-
-# Se importa el módulo
 import socket
+from threading import Thread
+import time
+from queue import Queue
+import threading
 
+class palabra:
+    def __init__(self, palabra, cantidad, IP, puerto):
+        self.palabra = palabra
+        self.cantidad = cantidad
+        self.IP=IP
+        self.puerto=puerto
+ 
+    def imp(self):
+        print("(" +self.palabra+" ,"+ str(self.cantidad)+" ,"+ str(self.IP)+" ,"+ str(self.puerto)+")")
+    def setcant(self,cantidad):
+        self.cantidad=cantidad
+    def getpalabra(self):
+        return self.palabra
+
+my_lista=[ ]
+
+def consola():
+    print("si desea que se desplieguen los resultados almacenados hasta el momento de una direccion IP y un puerto dato") 
+    print("digite una direccion IP y posteriormente se le solicitará un puerto ")
+    while True:
+        direccion= input()
+        print("ud ha digitado la dirección: "+str(direccion)+" digite un puerto: ")
+        puerto= input()
+        print("resultados... ")
+        resultados=0
+        for i in range(len(my_lista)):
+                    if str(my_lista[i].IP)==direccion and str(my_lista[i].puerto)==puerto: 
+                         my_lista[i].imp()
+                         resultados+=1
+        if resultados ==0:
+            print("No se encontraron resultados, verifique que la dirección IP y el puerto dado son correctos")
+        
 # instanciamos un objeto para trabajar con el socket
 ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -13,19 +47,49 @@ ser.listen(1)
 
 # Instanciamos un objeto cli (socket cliente) para recibir datos
 cli, addr = ser.accept()
+hiloConsola = Thread(target=consola, args=())
+hiloConsola.start()
 
-seguir = True
-while seguir:
-    # Recibimos el mensaje, con el metodo recv recibimos datos. Por parametro la cantidad de bytes para recibir
-    recibido = cli.recv(1024)
-    if recibido.decode() != "1":
-        # Si se reciben datos nos muestra la IP y el mensaje recibido
-        print("Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]))
-        print(recibido.decode())
-        # Devolvemos el mensaje al cliente
-        cli.send(recibido)
-    if recibido.decode() == "1":
-        print("conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]) + " ha sido cerrada")
-        # cli.close()
-        # Ser.close()
-        seguir = False
+semaforo = threading.Semaphore(0) 
+
+def recibir():
+    while True:
+        # Recibimos el mensaje, con el metodo recv recibimos datos. Por parametro la cantidad de bytes para recibir
+        recibido = cli.recv(1024)
+        if recibido.decode() != "1":
+            # Si se reciben datos nos muestra la IP y el mensaje recibido
+            print("Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]))
+            print(recibido.decode())
+            p=palabra(recibido.decode(),0,addr[0], addr[1])
+            my_lista.append(p)
+            # Devolvemos el mensaje al cliente
+            cli.send(recibido)
+            semaforo.release()
+        if recibido.decode() == "1":
+            semaforo.release()
+            print("estoy aqui")
+            break
+
+
+def contador():
+    actual=0
+    
+    while True:
+        h = my_lista[actual]
+        
+        if h.getpalabra()=="1":
+            print("conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1]) + " ha sido cerrada")
+            for i in range(len(my_lista)):
+                print(i)
+                cli.send(my_lista[i])
+            return 0
+        semaforo.acquire()   
+        x = len(h.getpalabra().split())
+        h.setcant(x)
+        actual+=1
+
+hiloRecibidor = Thread(target=recibir, args=())
+hiloContador = Thread(target=contador, args=())
+
+hiloRecibidor.start()
+hiloContador.start()
