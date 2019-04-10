b8a1cdb68be4bf498b7edb51a83a8b130bb3c0de
Berta <nosarabs@gmail.com>
2019-04-10 15:24:58 -0600

    Conectar( );

--- F2/P2/Receptor.cpp
+++ F2/P2/Receptor.cpp
@@ -9,12 +9,50 @@ Receptor::~Receptor(){
 }
 
 void Receptor::conectar(){
-    
+  int server_fd, new_socket;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+      perror("socket failed");
+      exit(EXIT_FAILURE);
+  }
+
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
+      perror("setsockopt");
+      exit(EXIT_FAILURE);
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons( PORT );
+
+  // Forcefully attaching socket to the port 8080
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0){
+      perror("bind failed");
+      exit(EXIT_FAILURE);
+  }
+
+  if (listen(server_fd, 3) < 0){
+      perror("listen");
+      exit(EXIT_FAILURE);
+  }
+
+  while(true){
+    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+    if (new_socket < 0){
+        perror("accept");
+        exit(EXIT_FAILURE);
+    } else {
+      printf("Conexión de la IP: ", address.sin_addr.s_addr, "Puerto: ", address.sin_port, " ha sido exitosa.");
+    }
+  }
 }
 
 void Receptor::desempaquetar(char * paquete){
-    
+
 }
 void Receptor::encolar(char * mensajeUtil, long id, int tamano){
-    
+
 }
--- F2/P2/Receptor.h
+++ F2/P2/Receptor.h
@@ -4,13 +4,14 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string>
 #include <pthread.h>
+#define PORT 9999
 /*El struct que utiliza el método que extrae los mensajes y escribe en los archivos
  */
 
@@ -21,19 +22,24 @@ class Receptor{
         /*constructor, inicializa cola de mensajes
          */
         Receptor();
+
         /*Destructor de la clase Receptor
          */
         ~Receptor();
-        /*Método que establece conexión con el emisor y recibe los mensajes 
+
+        /*Método que establece conexión con el emisor y recibe los mensajes
          */
         void conectar();
+
         /*método que extrae el id, el tamaño del mensaje y el mensaje util, los separa y envía a encolar
          */
         void desempaquetar(char *);
+
         /*Método que recibe el mensaje util y el id, y el tamaño del mensaje para proceder a encolarlo en la
          * cola con su respectivo mensaje, prioridad y tamaño
          */
         void encolar(char *, long, int);
+
         /*Método que utilizan los hilos para desencolar mensaje util y escribir en el archivo
          */
         static void* hiloEscritor(void * data){
@@ -45,10 +51,10 @@ class Receptor{
                 colaH->desencolar_Mensaje(idT);
                 if(strcmp(colaH->un_Mensaje.mensaje,"")!=0)
                     write( id, colaH->un_Mensaje.mensaje, colaH->un_Mensaje.tamano);
-			
+
             }while(strcmp(colaH->un_Mensaje.mensaje,"")!=0);
         }
-        
-    
+
+
 };
 #endif
